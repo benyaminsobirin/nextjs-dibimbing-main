47881224c46612892becc89ae42d8c8ff853f79b
benyaminsobirin <benyaminsobirin17@gmail.com>
2023-07-30 21:35:49 +0700

    items lorem

--- src/pages/items.jsx
+++ src/pages/items.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function items() {
-  return <div>items</div>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore beatae
+        illum omnis ad animi dicta nemo id? Assumenda ad, cum numquam, illum
+        doloremque doloribus nemo eligendi, amet placeat eaque et!
+      </h1>
+    </div>
+  );
 }
